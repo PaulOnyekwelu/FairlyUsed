ce1e5323c510275e1c34e6a5d61f517c33f459a3
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-11-29 08:34:45 +0100

    implemented apptext custom component

---
+++ app/components/AppText.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, Platform } from "react-native";
+import colors from "../configs/colors";
+
+const AppText = ({ children, style }) => {
+  return <Text style={[styles.text, style]}>{children}</Text>;
+};
+
+export default AppText;
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    color: colors.black,
+  },
+});
